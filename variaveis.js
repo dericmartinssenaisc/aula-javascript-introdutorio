5c58c97ddfa8e98a05634ff965f23c225bf91e68
dericmartinssenaisc <deric.martins@edu.sc.senai.br>
2025-09-10 19:30:50 -0300

    Diferenca de variaveis de portugol para Javascript

---
+++ variaveis.js
@@ -0,0 +1,22 @@
+// Portugol para JavaScript
+// Declaração de variáveis
+
+/* Em Portugol:
+cadeia nome = "João"
+cadeia sobrenome = "Silva"
+inteiro idade = 25
+real altura = 1.75
+logico ehHumano = verdadeiro
+*/
+
+let nome = "João"
+let sobrenome = "Silva"
+let idade = 25
+let altura = 1.75
+let ehHumano = true
+
+console.log("Nome:", nome) // escreva("Nome: ", nome)
+console.log("Sobrenome:", sobrenome) //escreva("Sobrenome: ", sobrenome)
+console.log("Idade:", idade) //escreva("Idade: ", idade)
+console.log("Altura:", altura) //escreva("Altura: ", altura)
+console.log("É humano?", ehHumano) //escreva("É humano? ", ehHumano)
