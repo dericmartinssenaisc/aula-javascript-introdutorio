8220f2827b5ab61a4097bcd9341cdb34b37d2eeb
Professor Déric Martins <deric.martins@edu.sc.senai.br>
2025-09-10 23:43:12 +0000

    Adiciona arquivo exercicio-pratico-um.js com informações pessoais e logs de perfil

---
+++ exercicio-pratico-um.js
@@ -0,0 +1,9 @@
+let meuNome = "Déric Martins";
+let minhaIdade = 29;
+let minhaCidade = "Alta Floresta - MT";
+let gostaDeProgramar = true;
+
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos!`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log("Gosta de Programar: ", gostaDeProgramar);
