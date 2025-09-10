2686910e7ab9a9e12ccd5a6a29cddf4f7e9d4c06
Professor Déric Martins <deric.martins@edu.sc.senai.br>
2025-09-10 23:53:58 +0000

    Adiciona estrutura de condicionais em JavaScript com exemplos de aprovação e reprovação

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+/*
+    se (nota >= 7.) {
+        escreva("aprovado!")
+    } senao {
+      escreva("Reprovada")
+    }
+*/
+
+let nota = 7.0;
+
+if (nota >= 7.0) {
+    console.log("Aprovado!");
+} else {
+    console.log("Reprovado!");
+}
+
+console.log(typeof nota)
